51161b2a72e857dd809d8e59c1bd3d081e8f89b0
svensktutby <svensk.tut.by@gmail.com>
2021-01-05 21:56:53 +0300

    refactor: change Users FC to class  component
    (lesson 54)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import s from './Users.module.css';
 import { UserType } from '../../types/types';
@@ -20,76 +20,77 @@ type GetUsersResponseType = {
   error: string | null;
 };
 
-export const Users: FC<StatePropsType & DispatchPropsType> = ({
-  users,
-  follow,
-  unfollow,
-  setUsers,
-}) => {
-  const getUsers = () => {
+type PropsType = StatePropsType & DispatchPropsType;
+
+type StateType = {};
+
+export class Users extends Component<PropsType, StateType> {
+  componentDidMount() {
     const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
 
-    if (!users.length) {
-      axios({
-        method: 'GET',
-        url: `${baseUrl}/users?count=4`,
-      }).then((res: AxiosResponse<GetUsersResponseType>) => {
-        setUsers(res.data.items);
-      });
-    }
-  };
+    axios({
+      method: 'GET',
+      url: `${baseUrl}/users?count=4`,
+    }).then((res: AxiosResponse<GetUsersResponseType>) => {
+      this.props.setUsers(res.data.items);
+    });
+  }
 
-  const userElements = users.map((u) => (
-    <li className={s.item} key={u.id}>
-      <div>
-        <div className={s.avatarWrapper}>
-          <img
-            className={s.avatar}
-            src={u.photos.small ? u.photos.small : userAvatar}
-            width="50"
-            height="50"
-            alt="User avatar"
-          />
-        </div>
-        <div>
-          {u.followed ? (
-            <button
-              className={s.followBtn}
-              type="button"
-              onClick={() => follow(u.id)}
-            >
-              Unfollow
-            </button>
-          ) : (
-            <button
-              className={s.followBtn}
-              type="button"
-              onClick={() => unfollow(u.id)}
-            >
-              Follow
-            </button>
-          )}
-        </div>
-      </div>
-      <div className={s.info}>
-        <div>
-          <div className={s.name}>{u.name}</div>
-          <div className={s.status}>{u.status}</div>
-        </div>
-        <div className={s.location}>
-          <div>{'u.location.country'},</div>
-          <div>{'u.location.city'}</div>
-        </div>
-      </div>
-    </li>
-  ));
+  render() {
+    const { users, follow, unfollow } = this.props;
 
-  return (
-    <div>
-      <ul className={s.list}>{userElements}</ul>
-      <button className={s.moreBtn} type="button" onClick={() => getUsers()}>
-        Show more
-      </button>
-    </div>
-  );
-};
+    const userElements = users.map((u) => (
+      <li className={s.item} key={u.id}>
+        <div>
+          <div className={s.avatarWrapper}>
+            <img
+              className={s.avatar}
+              src={u.photos.small ? u.photos.small : userAvatar}
+              width="50"
+              height="50"
+              alt="User avatar"
+            />
+          </div>
+          <div>
+            {u.followed ? (
+              <button
+                className={s.followBtn}
+                type="button"
+                onClick={() => follow(u.id)}
+              >
+                Unfollow
+              </button>
+            ) : (
+              <button
+                className={s.followBtn}
+                type="button"
+                onClick={() => unfollow(u.id)}
+              >
+                Follow
+              </button>
+            )}
+          </div>
+        </div>
+        <div className={s.info}>
+          <div>
+            <div className={s.name}>{u.name}</div>
+            <div className={s.status}>{u.status}</div>
+          </div>
+          <div className={s.location}>
+            <div>{'u.location.country'},</div>
+            <div>{'u.location.city'}</div>
+          </div>
+        </div>
+      </li>
+    ));
+
+    return (
+      <div className={s.usersBlock}>
+        <ul className={s.list}>{userElements}</ul>
+        <button className={s.moreBtn} type="button" onClick={() => {}}>
+          Show more
+        </button>
+      </div>
+    );
+  }
+}
