303158c7ae7ad9314ab6290ad2e42e9ed1c7abef
svensktutby <svensk.tut.by@gmail.com>
2021-01-11 18:32:17 +0300

    refactor: move fetch requests into UsersContainer
    (lesson 56)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,142 +1,100 @@
-import React, { Component } from 'react';
-import axios, { AxiosResponse } from 'axios';
+import React, { FC } from 'react';
 import s from './Users.module.css';
-import { UserType } from '../../types/types';
 import userAvatar from '../../assets/images/userAvatar.svg';
 import { randomId } from '../../utils/randomId';
+import { UserType } from '../../types/types';
 
-export type StatePropsType = {
+type UsersPropsType = {
   users: Array<UserType>;
   pageSize: number;
   totalUsersCount: number;
   currentPage: number;
-};
-
-export type DispatchPropsType = {
   follow: (payload: number) => void;
   unfollow: (payload: number) => void;
-  setUsers: (payload: Array<UserType>) => void;
-  setCurrentPage: (payload: number) => void;
-  setUsersTotalCount: (payload: number) => void;
+  changePageHandler: (payload: number) => void;
 };
 
-type GetUsersResponseType = {
-  items: Array<UserType>;
-  totalCount: number;
-  error: string | null;
-};
+export const Users: FC<UsersPropsType> = (props) => {
+  const {
+    users,
+    pageSize,
+    totalUsersCount,
+    currentPage,
+    follow,
+    unfollow,
+    changePageHandler,
+  } = props;
 
-type PropsType = StatePropsType & DispatchPropsType;
-
-type StateType = {};
-
-export class Users extends Component<PropsType, StateType> {
-  componentDidMount() {
-    const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
-
-    axios({
-      method: 'GET',
-      url: `${baseUrl}/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-    }).then((res: AxiosResponse<GetUsersResponseType>) => {
-      this.props.setUsers(res.data.items);
-      this.props.setUsersTotalCount(res.data.totalCount);
-    });
-  }
-
-  changePageHandler = (pageNumber: number) => {
-    this.props.setCurrentPage(pageNumber);
-
-    const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
-
-    axios({
-      method: 'GET',
-      url: `${baseUrl}/users?page=${pageNumber}&count=${this.props.pageSize}`,
-    }).then((res: AxiosResponse<GetUsersResponseType>) => {
-      this.props.setUsers(res.data.items);
-    });
-  };
-
-  render() {
-    const {
-      users,
-      pageSize,
-      totalUsersCount,
-      currentPage,
-      follow,
-      unfollow,
-    } = this.props;
-
-    const userElements = users.map((u) => (
-      <li className={s.item} key={u.id}>
-        <div>
-          <div className={s.avatarWrapper}>
-            <img
-              className={s.avatar}
-              src={u.photos.small ? u.photos.small : userAvatar}
-              width="50"
-              height="50"
-              alt="User avatar"
-            />
-          </div>
-          <div>
-            {u.followed ? (
-              <button
-                className={s.followBtn}
-                type="button"
-                onClick={() => follow(u.id)}
-              >
-                Unfollow
-              </button>
-            ) : (
-              <button
-                className={s.followBtn}
-                type="button"
-                onClick={() => unfollow(u.id)}
-              >
-                Follow
-              </button>
-            )}
-          </div>
+  const userElements = users.map((u) => (
+    <li className={s.item} key={u.id}>
+      <div>
+        <div className={s.avatarWrapper}>
+          <img
+            className={s.avatar}
+            src={u.photos.small ? u.photos.small : userAvatar}
+            width="50"
+            height="50"
+            alt="User avatar"
+          />
         </div>
-        <div className={s.info}>
-          <div>
-            <div className={s.name}>{u.name}</div>
-            <div className={s.status}>{u.status}</div>
-          </div>
-          <div className={s.location}>
-            <div>{'u.location.country'},</div>
-            <div>{'u.location.city'}</div>
-          </div>
-        </div>
-      </li>
-    ));
-
-    const pagesCount = Math.ceil(totalUsersCount / pageSize);
-
-    const pages = Array(Math.min(10, pagesCount)).fill(null);
-
-    return (
-      <div className={s.usersBlock}>
         <div>
-          {pages.map((p, idx) => (
+          {u.followed ? (
             <button
+              className={s.followBtn}
               type="button"
-              key={randomId()}
-              className={`${s.pageBtn} ${
-                idx + 1 === currentPage && s.selectedPage
-              }`}
-              onClick={() => this.changePageHandler(idx + 1)}
+              onClick={() => follow(u.id)}
             >
-              {idx + 1}
+              Unfollow
             </button>
-          ))}
+          ) : (
+            <button
+              className={s.followBtn}
+              type="button"
+              onClick={() => unfollow(u.id)}
+            >
+              Follow
+            </button>
+          )}
         </div>
-
-        <ul className={s.list}>{userElements}</ul>
-        <button className={s.moreBtn} type="button" onClick={() => {}}>
-          Show more
-        </button>
       </div>
-    );
-  }
-}
+      <div className={s.info}>
+        <div>
+          <div className={s.name}>{u.name}</div>
+          <div className={s.status}>{u.status}</div>
+        </div>
+        <div className={s.location}>
+          <div>{'u.location.country'},</div>
+          <div>{'u.location.city'}</div>
+        </div>
+      </div>
+    </li>
+  ));
+
+  const pagesCount = Math.ceil(totalUsersCount / pageSize);
+
+  const pages = Array(Math.min(10, pagesCount)).fill(null);
+
+  return (
+    <div className={s.usersBlock}>
+      <div>
+        {pages.map((p, idx) => (
+          <button
+            type="button"
+            key={randomId()}
+            className={`${s.pageBtn} ${
+              idx + 1 === currentPage && s.selectedPage
+            }`}
+            onClick={() => changePageHandler(idx + 1)}
+          >
+            {idx + 1}
+          </button>
+        ))}
+      </div>
+
+      <ul className={s.list}>{userElements}</ul>
+      <button className={s.moreBtn} type="button" onClick={() => {}}>
+        Show more
+      </button>
+    </div>
+  );
+};
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,6 +1,7 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios, { AxiosResponse } from 'axios';
 
-import { DispatchPropsType, StatePropsType, Users } from './Users';
 import { AppStateType } from '../../redux/reduxStore';
 import {
   follow,
@@ -9,6 +10,81 @@ import {
   setUsersTotalCount,
   unfollow,
 } from '../../redux/actions';
+import { UserType } from '../../types/types';
+import { Users } from './Users';
+
+type StatePropsType = {
+  users: Array<UserType>;
+  pageSize: number;
+  totalUsersCount: number;
+  currentPage: number;
+};
+
+type DispatchPropsType = {
+  follow: (payload: number) => void;
+  unfollow: (payload: number) => void;
+  setUsers: (payload: Array<UserType>) => void;
+  setCurrentPage: (payload: number) => void;
+  setUsersTotalCount: (payload: number) => void;
+};
+
+type GetUsersResponseType = {
+  items: Array<UserType>;
+  totalCount: number;
+  error: string | null;
+};
+
+type PropsType = StatePropsType & DispatchPropsType;
+
+class UsersAPIContainer extends Component<PropsType> {
+  componentDidMount() {
+    const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
+
+    axios({
+      method: 'GET',
+      url: `${baseUrl}/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+    }).then((res: AxiosResponse<GetUsersResponseType>) => {
+      this.props.setUsers(res.data.items);
+      this.props.setUsersTotalCount(res.data.totalCount);
+    });
+  }
+
+  changePageHandler = (pageNumber: number) => {
+    this.props.setCurrentPage(pageNumber);
+
+    const baseUrl = 'https://social-network.samuraijs.com/api/1.0';
+
+    axios({
+      method: 'GET',
+      url: `${baseUrl}/users?page=${pageNumber}&count=${this.props.pageSize}`,
+    }).then((res: AxiosResponse<GetUsersResponseType>) => {
+      this.props.setUsers(res.data.items);
+    });
+  };
+
+  render() {
+    const {
+      users,
+      pageSize,
+      totalUsersCount,
+      currentPage,
+      follow,
+      unfollow,
+    } = this.props;
+
+    return (
+      <Users
+        users={users}
+        pageSize={pageSize}
+        totalUsersCount={totalUsersCount}
+        currentPage={currentPage}
+        follow={follow}
+        unfollow={unfollow}
+        changePageHandler={this.changePageHandler}
+      />
+    );
+  }
+}
 
 const mapStateToProps = ({
   usersPage: { users, pageSize, totalUsersCount, currentPage },
@@ -32,4 +108,4 @@ export const UsersContainer = connect<
   setUsers,
   setCurrentPage,
   setUsersTotalCount,
-})(Users);
+})(UsersAPIContainer);
