073166e012d855a0e1cead59df0f2435c0305b08
svensktutby <svensk.tut.by@gmail.com>
2021-03-30 08:07:37 +0300

    feat: add custom favicon

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -8,7 +8,7 @@ type PreloaderPropsType = {
 
 export const Preloader: FC<PreloaderPropsType> = ({ text }) => {
   return (
-    <div className={s.wrapper}>
+    <div className={s.preloader}>
       <div className={s.loaderCircle} />
       <div className={s.loaderBottom}>{text}</div>
     </div>
