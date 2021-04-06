3d98609f898900cf71a1d66ba6015d8ba707fc66
svensktutby <svensk.tut.by@gmail.com>
2021-04-06 12:28:54 +0300

    refactor: change add user profile photo input styles

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent, FC } from 'react';
 
 import s from './ProfileInfo.module.css';
-import styleBtn from '../../common/styles/button.module.css';
 import userPhoto from '../../../assets/images/userAvatar.svg';
 import { ProfileType } from '../../../types/types';
 import { Preloader } from '../../common/Preloader/Preloader';
@@ -40,20 +39,27 @@ export const ProfileInfo: FC<ProfileInfoPropsType> = ({
           src={profile.photos.large || userPhoto}
           alt={`${profile.fullName}`}
         />
-      </div>
 
-      {isOwner && (
-        <div className={s.btnWrapper}>
-          <label className={styleBtn.btn}>
+        {isOwner && (
+          <label className={s.label} title="Add photo">
             <input
               className={s.inputFile}
               type="file"
               onChange={addUserPhotoHandler}
             />
-            <span>Add photo</span>
+            <svg
+              className={s.icon}
+              viewBox="0 0 32 32"
+              width="32"
+              version="1.1"
+              height="32"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z" />
+            </svg>
           </label>
-        </div>
-      )}
+        )}
+      </div>
 
       <ProfileStatus status={status} updateStatus={updateStatus} />
     </div>
